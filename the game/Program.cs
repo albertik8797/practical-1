70ba6629bf9c83b7a435097a27533c58e8b49598
Альберт Щелёв <115091513+albertik8797@users.noreply.github.com>
2022-11-02 11:14:11 +0300

    лрлр

--- the game/Program.cs
+++ the game/Program.cs
@@ -1,82 +1,192 @@
-﻿namespace GuessNumber
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
 {
     class Program
     {
-        static void Ugad()
+        static string[] menuItems;
+        static string[] menuItems1;
+        static int counter = 0;
+        static int counter1 = 0;
+        struct notes
         {
-            int myNum;
-            Console.Title = " Угадай число ";
-            Random rand = new Random();
-            string temp;
-            int secret = rand.Next(100);
-            Console.WriteLine("Я загодал число от 0 до 100 Попробуйте его отгадать.");
-            while (true)
+            public DateTime date;
+            public string title;
+            public string descriptions;
+
+        }
+        static notes[] note = new notes[6];
+
+        //вывод информации о заметках
+        static void printnote(string d, string e)
+        {
+            Console.Clear();
+            if (d == "Назад")
+                printmenu();
+            else
             {
-                Console.Write("Введите число: ");
-                temp = Console.ReadLine();
-                if (!int.TryParse(temp, out int myNam))
+                for (int i = 0; i < note.Length; i++)
                 {
-                    continue;
-                }
-                if (myNam == secret)
-                {
-                    Console.WriteLine("Угадали.");
-                    break;
-                }
-                else
-                {
-                    if (myNam > secret)
+                    if (note[i].title == d)
                     {
-                        Console.WriteLine("Моe число меньше твоего.");
+                        Console.WriteLine((note[i].date).ToString("dd.MM.yyyy"));
+                        Console.WriteLine(note[i].title);
+                        Console.WriteLine(note[i].descriptions);
+
+                    }
+                }
+            }
+            ConsoleKeyInfo key;
+            key = Console.ReadKey(true);
+            switch (key.Key)
+            {
+                case ConsoleKey.Enter:
+                    printmenu1(e);
+                    break;
+
+            }
+
+
+
+        }
+
+        //меню дат
+        static int printmenu()
+        {
+            counter = 0;
+
+
+            menuItems = new string[3];
+            menuItems[0] = "11.11.2022";
+            menuItems[1] = "15.11.2022";
+            menuItems[2] = "22.11.2022";
+
+            ConsoleKeyInfo key;
+            do
+            {
+                Console.Clear();
+                for (int i = 0; i < menuItems.Length; i++)
+                {
+                    if (counter == i)
+                    {
+
+                        Console.WriteLine("→" + menuItems[i]);
+
                     }
                     else
+                        Console.WriteLine(" " + menuItems[i]);
+                }
+
+                key = Console.ReadKey(true);
+                switch (key.Key)
+                {
+                    case ConsoleKey.Enter:
+                        printmenu1(menuItems[counter]);
+                        return counter;
+                    case ConsoleKey.UpArrow:
+                        counter--;
+                        if (counter == -1)
+                            counter = menuItems.Length - 1;
+                        break;
+                    case ConsoleKey.DownArrow:
+                        counter++;
+                        if (counter == menuItems.Length)
+                            counter = 0;
+                        break;
+                }
+            }
+            while (true);
+
+        }
+
+        //меню названий
+        static int printmenu1(string d)
+        {
+            counter1 = 0;
+
+            menuItems1 = new string[6];
+            int k = 0;
+            for (int i = 0; i < note.Length; i++)
+            {
+                if ((note[i].date).ToString("dd.MM.yyyy") == d)
+                {
+
+                    menuItems1[k] = note[i].title;
+                    k++;
+                }
+            }
+            menuItems1[k] = "Назад";
+            k++;
+
+            ConsoleKeyInfo key;
+            do
+            {
+                Console.Clear();
+
+
+
+                for (int i = 0; i < k; i++)
+                {
+
+                    if (counter1 == i)
                     {
-                        Console.WriteLine("Моe число больше твоего.");
+
+                        Console.WriteLine("→" + menuItems1[i]);
+
                     }
+                    else
+                        Console.WriteLine(" " + menuItems1[i]);
+
+
+                }
+
+                key = Console.ReadKey(true);
+                switch (key.Key)
+                {
+                    case ConsoleKey.Enter:
+                        printnote(menuItems1[counter1], d);
+                        return counter1;
+                    case ConsoleKey.UpArrow:
+                        counter1--;
+                        if (counter1 == -1)
+                            counter1 = k - 1;
+                        break;
+                    case ConsoleKey.DownArrow:
+                        counter1++;
+                        if (counter1 == k)
+                            counter1 = 0;
+                        break;
                 }
             }
+            while (true);
+
         }
-        static void tabl()
+        static void Main(string[] args)
         {
-            int[,] myltiplytable = new int[10, 10];
-            for (int i = 1; i < 10; i++)
-                for (int j = 1; j < 10; j++)
-                    myltiplytable[i, j] = i * j;
-            for (int i = 1; i < 10; i++)
-            {
-                Console.WriteLine();
-                for (int j = 1; j < 10; j++)
-                    Console.Write("{0,3}", myltiplytable[i, j] + "\t");
-            }
-        }
-        static void Main()
-        {
-            while (true)
-            {
-                Console.WriteLine("\nВыбери программу:\n1:Угадай число\n2:Таблица умножения\n3:Выйти");
-                string oper;
-                oper = Console.ReadLine();
-                if (!int.TryParse(oper, out int myNam))
-                {
-                    Console.WriteLine("Введи число");
-                }
-                if (myNam == 1)
-                {
-                    Ugad();
-                }
-                else if (myNam == 2)
-                {
-                    tabl();
-                }
-                else if (myNam == 3)
-                {
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine("Выбери приложение из списка");
-                }
-            }
+            //int v;
+
+            note[0].date = Convert.ToDateTime("11.11.2022");
+            note[0].title = "Дела";
+            note[0].descriptions = "Погулять с собакой";
+
+            note[1].date = Convert.ToDateTime("10.11.2022");
+            note[1].title = "Кот";
+            note[1].descriptions = "День рождение кота";
+
+            note[2].date = Convert.ToDateTime("9.11.2022");
+            note[2].title = "Почта";
+            note[2].descriptions = "Забрать посылку";
+
+            note[3].date = Convert.ToDateTime("8.11.2022");
+            note[3].title = "Лес";
+            note[3].descriptions = "Прогулка с друзьями";
+
+
+            note[4].date = Convert.ToDateTime("7.11.2022");
+            note[4].title = "Курсовая работа";
+            note[4].descriptions = "Начать делать курсовую работу";
+            printmenu();
         }
     }
 }
